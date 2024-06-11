41a9844d9f724cff6684ed1d7f6a70393db7a50e
Marek Kopania <234760@edu.p.lodz.pl>
2024-06-11 19:07:09 +0200

    update: microservices setup with api gateway and discovery server

--- Backend/users/src/main/java/com/musicstore/users/controller/RegisterController.java
+++ Backend/users/src/main/java/com/musicstore/users/controller/RegisterController.java
@@ -7,6 +7,7 @@ import com.musicstore.users.service.LoginService;
 import com.musicstore.users.service.RegisterService;
 import com.musicstore.users.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,26 +22,31 @@ public class RegisterController {
     private final UserService userService;
 
     @PostMapping(path = "register")
+    @ResponseStatus(HttpStatus.CREATED)
     public String register(@RequestBody RegisterRequest request) {
         return registerService.register(request);
     }
 
     @GetMapping(path = "register/confirm")
+    @ResponseStatus(HttpStatus.OK)
     public String confirm(@RequestParam("token") String token) {
         return registerService.confirmToken(token);
     }
 
     @PostMapping(path = "login")
+    @ResponseStatus(HttpStatus.OK)
     public LoginResponse login(@RequestBody LoginRequest request) {
         return loginService.loginUser(request);
     }
 
     @PutMapping(path = "update/{uuid}")
+    @ResponseStatus(HttpStatus.OK)
     public String updateUser(@PathVariable("uuid") UUID uuid, @RequestBody RegisterRequest request) {
         return userService.updateUser(uuid, request);
     }
 
     @DeleteMapping(path = "delete/{uuid}")
+    @ResponseStatus(HttpStatus.OK)
     public String deleteUser(@PathVariable("uuid") UUID uuid) {
         return userService.deleteUser(uuid);
     }

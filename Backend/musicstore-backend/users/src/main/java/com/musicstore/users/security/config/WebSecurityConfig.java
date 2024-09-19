45cda7a09836f293ea65eeb8d65a2eabe2c4c36d
Marek Kopania <234760@edu.p.lodz.pl>
2024-09-19 17:20:43 +0200

    update: wrapped entire project into parent module

--- Backend/musicstore-backend/users/src/main/java/com/musicstore/users/security/config/WebSecurityConfig.java
+++ Backend/musicstore-backend/users/src/main/java/com/musicstore/users/security/config/WebSecurityConfig.java
@@ -23,8 +23,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity

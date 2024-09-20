1c6ea0a9112da0e32405d3c0d576da2ca10fbdb4
Marek Kopania <234760@edu.p.lodz.pl>
2024-09-21 00:39:54 +0200

    update: navbar setup for later dev

--- Frontend/musicstore-frontend/src/components/navbar/Navbar.jsx
+++ Frontend/musicstore-frontend/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './style/Navbar.scss';
 import LocalStorageHelper from "../../helpers/LocalStorageHelper.jsx";
 import logo from '../../assets/logo.svg';
+import Tooltip from '@mui/material/Tooltip';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
@@ -35,7 +36,6 @@ function Navbar() {
                             className="search-input"
                             placeholder="Search products"
                             required
-                            pattern="[a-zA-Z0-9][a-zA-Z0-9]*"
                             value={serach}
                             onChange={e => setSerach(e.target.value)}
                         />
@@ -51,57 +51,69 @@ function Navbar() {
                     <ul>
                         {LocalStorageHelper.IsUserLogged() === true &&
                             <li>
-                                <Link to="/account">
-                                    <div className={"welcome-text"}>
-                                        <p>
-                                            Hi,<br/>{LocalStorageHelper.getUserName()}
-                                        </p>
-                                    </div>
-                                    <PersonOutlineOutlinedIcon fontSize={"medium"}/>
-                                </Link>
+                                <Tooltip title={"My account"}>
+                                    <Link to="/account">
+                                        <div className={"welcome-text"}>
+                                            <p>
+                                                Hi,<br/>{LocalStorageHelper.getUserName()}
+                                            </p>
+                                        </div>
+                                        <PersonOutlineOutlinedIcon fontSize={"medium"}/>
+                                    </Link>
+                                </Tooltip>
                             </li>
                         }
                         {LocalStorageHelper.IsUserLogged() === false &&
                             <li>
-                                <Link to="/login">
-                                    <div className={"welcome-text"}>
-                                        <p>
-                                            Hi,<br/>Login
-                                        </p>
-                                    </div>
-                                    <PersonOutlineOutlinedIcon fontSize={"medium"}/>
-                                </Link>
+                                <Tooltip title={"Login"}>
+                                    <Link to="/login">
+                                        <div className={"welcome-text"}>
+                                            <p>
+                                                Hi,<br/>Login
+                                            </p>
+                                        </div>
+                                        <PersonOutlineOutlinedIcon fontSize={"medium"}/>
+                                    </Link>
+                                </Tooltip>
                             </li>
                         }
                         <li>
-                            <Link to="/basket">
-                                <ShoppingCartOutlinedIcon fontSize={"medium"}/>
-                            </Link>
+                            <Tooltip title={"Basket"}>
+                                <Link to="/basket">
+                                    <ShoppingCartOutlinedIcon fontSize={"medium"}/>
+                                </Link>
+                            </Tooltip>
                         </li>
 
                             <li>
-                                <Link to="/admin">
-                                    <div className={"admin-text"}>
-                                        <p>
-                                            Admin<br/>panel
-                                        </p>
-                                    </div>
-                                    <AdminPanelSettingsOutlinedIcon fontSize={"medium"}/>
-                                </Link>
+                                <Tooltip title={"Admin panel"}>
+                                    <Link to="/admin">
+                                        <div className={"admin-text"}>
+                                            <p>
+                                                Admin<br/>panel
+                                            </p>
+                                        </div>
+                                        <AdminPanelSettingsOutlinedIcon fontSize={"medium"}/>
+                                    </Link>
+                                </Tooltip>
                             </li>
 
                         {LocalStorageHelper.IsUserLogged() === false &&
                             <li>
-                                <Link to="/login">
-                                <LoginOutlinedIcon fontSize={"medium"}/>
-                                </Link>
+                                <Tooltip title={"Login"}>
+                                    <Link to="/login">
+                                        <LoginOutlinedIcon fontSize={"medium"}/>
+                                    </Link>
+                                </Tooltip>
                             </li>
                         }
                         {LocalStorageHelper.IsUserLogged() === true &&
                             <li>
-                                <Link onClick={logoutUser}>
-                                    <LogoutOutlinedIcon fontSize={"medium"}/>
-                                </Link>
+                                <Tooltip title={"Logout"}>
+                                    <Link onClick={logoutUser}>
+                                        <LogoutOutlinedIcon fontSize={"medium"}/>
+                                    </Link>
+                                </Tooltip>
                             </li>
                         }
                     </ul>

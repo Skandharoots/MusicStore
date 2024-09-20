17619c4e4cd5f457282ad27b225196e6a62cbdb8
Marek Kopania <234760@edu.p.lodz.pl>
2024-09-20 23:03:19 +0200

    update: navbar setup for later dev

--- Frontend/musicstore-frontend/src/components/navbar/Navbar.jsx
+++ Frontend/musicstore-frontend/src/components/navbar/Navbar.jsx
@@ -4,10 +4,16 @@ import logo from '../../assets/logo.svg';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 
 function Navbar() {
 
     const [serach, setSerach] = React.useState('');
+
+    const logoutUser = () => {
+        //
+    }
 
     const onSubmitSearch = () => {
         //redirect
@@ -17,7 +23,7 @@ function Navbar() {
         <header>
             <div className={"top-container"}>
                 <a href="/">
-                    <img className="logo" src={logo} />
+                    <img alt={""} className="logo" src={logo}/>
                 </a>
                 <div className={"search-container"}>
                     <form className={"search-form"}>
@@ -29,32 +35,48 @@ function Navbar() {
                             pattern="[a-zA-Z0-9][a-zA-Z0-9]*"
                             value={serach}
                             onChange={e => setSerach(e.target.value)}
-                            />
+                        />
                         <button
                             type="submit"
                             className="search-btn"
                             onSubmit={onSubmitSearch}>
-                            <SearchOutlinedIcon fontSize={"small"} />
+                            <SearchOutlinedIcon fontSize={"small"}/>
                         </button>
                     </form>
                 </div>
                 <div className={"user-info-container"}>
                     <ul>
                         <li>
-                            <div className={"welcome-text"}>
-                                <p>
-                                    Welcome<br/>User
-                                </p>
-                            </div>
-                            <PersonOutlineOutlinedIcon fontSize={"medium"} />
+                            <a href="/account">
+                                <div className={"welcome-text"}>
+                                    <p>
+                                        Your<br/>Account
+                                    </p>
+                                </div>
+                                <PersonOutlineOutlinedIcon fontSize={"medium"}/>
+                            </a>
                         </li>
-                        <li><ShoppingCartOutlinedIcon fontSize={"medium"} /></li>
+                        <li>
+                            <a href="/basket">
+                                <ShoppingCartOutlinedIcon fontSize={"medium"}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/login">
+                                <LoginOutlinedIcon fontSize={"medium"}/>
+                            </a>
+                        </li>
+                        <li>
+                            <a onClick={logoutUser}>
+                                <LogoutOutlinedIcon fontSize={"medium"}/>
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
             <nav className="bottom-container">
                 <div>
-                <ul>
+                    <ul>
                         <li><a href="/">Guitars</a></li>
                         <li><a href="/">Drums</a></li>
                         <li><a href="/">Sound</a></li>

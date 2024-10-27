2aca7c7a382644b39a69422d093667f9c38c5ee3
Marek Kopania <234760@edu.p.lodz.pl>
2024-10-27 19:51:21 +0100

    update: backend PMD fixes

--- Backend/musicstore-backend/products/src/main/java/com/musicstore/products/service/SubcategoryService.java
+++ Backend/musicstore-backend/products/src/main/java/com/musicstore/products/service/SubcategoryService.java
@@ -9,8 +9,6 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;

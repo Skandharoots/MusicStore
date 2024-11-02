c979b29074e7c7454e8f76d94a8b2a574e5eadd6
Marek Kopania <234760@edu.p.lodz.pl>
2024-11-02 21:48:25 +0100

    update: grid styling fixed

--- Frontend/musicstore-frontend/src/pages/basket/Basket.jsx
+++ Frontend/musicstore-frontend/src/pages/basket/Basket.jsx
@@ -10,7 +10,7 @@ import {useNavigate} from "react-router-dom";
 
 function Basket() {
 
-    const [basketItems, setBasketItems] = useState([]);
+    const [basketItems, setBasketItems] = useState([{}]);
     const [totalCost, setTotalCost] = useState(0);
     const [open, setOpen] = useState(false);
 

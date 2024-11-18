3b529388c92bb90c5b1d0ae1a554d88f5f1156fc
Marek Kopania <234760@edu.p.lodz.pl>
2024-11-18 18:00:40 +0100

    update: frontend - final fixes for css, toast and form validation, all done

--- Frontend/musicstore-frontend/src/pages/signup/SignupConfirm.jsx
+++ Frontend/musicstore-frontend/src/pages/signup/SignupConfirm.jsx
@@ -52,9 +52,9 @@ function SignupConfirm() {
         } else {
             return (
                 <Button color="inherit" size="small" onClick={() => {
-                    navigate('/login')
+                    navigate('/signup')
                 }}>
-                    Login
+                    Register
                 </Button>
             )
         }
